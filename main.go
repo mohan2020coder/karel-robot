0d8260428088b4f7ab8c1062b6b7b02028f09864
mohan2020coder <mohan18.welcome@gmail.com>
2024-06-11 14:36:45 +0530

    not working

--- main.go
+++ main.go
@@ -1,20 +1,83 @@
 package main
 
 import (
-    "fyne.io/fyne/v2"
-    "fyne.io/fyne/v2/app"
-    "github.com/mohan2020coder/karel-robot/world"
-    "github.com/mohan2020coder/karel-robot/actions"
-    "github.com/mohan2020coder/karel-robot/gui"
-)
-//this is the mai master
-func main() {
-    world := NewWorld(10, 10) // Create a world with 10 rows and 10 columns
-    world.PutBeeper(Position{X: 5, Y: 3}) // Place a beeper at position (5, 3)
+	"image/color"
 
-    a := app.New()
-    w := a.NewWindow("Karel the Robot")
-    w.SetContent(createInterface(world, a))
-    w.Resize(fyne.NewSize(600, 600))
-    w.ShowAndRun()
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
+)
+
+type Karel struct {
+	X, Y int
+	Dir  Direction
+}
+
+type Direction int
+
+const (
+	North Direction = iota
+	East
+	South
+	West
+)
+
+var dirSymbols = [...]rune{'^', '>', 'v', '<'}
+
+func (k *Karel) TurnLeft() {
+	k.Dir = (k.Dir + 3) % 4
+}
+
+func (k *Karel) TurnRight() {
+	k.Dir = (k.Dir + 1) % 4
+}
+
+func (k *Karel) Move() {
+	switch k.Dir {
+	case North:
+		k.Y--
+	case South:
+		k.Y++
+	case West:
+		k.X--
+	case East:
+		k.X++
+	}
+}
+
+func main() {
+	myApp := app.New()
+	win := myApp.NewWindow("Karel the Robot")
+
+	karel := &Karel{X: 0, Y: 0, Dir: East}
+
+	karelCanvas := canvas.NewText(string(dirSymbols[karel.Dir]), color.Black)
+
+	grid := container.NewGridWithColumns(2,
+		canvas.NewRectangle(color.White),
+		container.NewVBox(
+			karelCanvas,
+			widget.NewButton("Move", func() {
+				karel.Move()
+				karelCanvas.Text = string(dirSymbols[karel.Dir])
+				win.Canvas().Refresh(karelCanvas)
+			}),
+			widget.NewButton("Turn Left", func() {
+				karel.TurnLeft()
+				karelCanvas.Text = string(dirSymbols[karel.Dir])
+				win.Canvas().Refresh(karelCanvas)
+			}),
+			widget.NewButton("Turn Right", func() {
+				karel.TurnRight()
+				karelCanvas.Text = string(dirSymbols[karel.Dir])
+				win.Canvas().Refresh(karelCanvas)
+			}),
+		),
+	)
+
+	win.SetContent(grid)
+	win.Resize(fyne.NewSize(400, 400))
+	win.ShowAndRun()
 }
